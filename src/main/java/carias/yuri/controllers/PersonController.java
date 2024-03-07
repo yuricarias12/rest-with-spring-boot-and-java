62a1ef02171497dc8f143d2f104ff7cf750e2492
Yuri Carias <yuristricken7@gmail.com>
2024-03-07 17:03:06 -0300

    Adicionado Migrations com Flyway ao projeto

--- src/main/java/carias/yuri/controllers/PersonController.java
+++ src/main/java/carias/yuri/controllers/PersonController.java
@@ -19,7 +19,7 @@ import carias.yuri.data.vo.v2.PersonVOV2;
 import carias.yuri.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,2 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Konoha', 'Sasuke', 'Male', 'Uchiha');
